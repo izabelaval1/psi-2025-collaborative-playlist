ea19cfe6a21431fa402f7c5322f7c72660c4bdf2
Izabela <izabelaval6@gmail.com>
2025-09-17 00:51:16 +0300

    Converted CSS to SCSS and added playlist display

--- music_app/src/App.tsx
+++ music_app/src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+  const playlists = [
+    { id: 1, name: 'Chill Vibes', description: 'Relaxing music' },
+    { id: 2, name: 'Workout', description: 'Pump up songs' },
+  ];
+
+  const displayPlaylists = () => {
+    return playlists.map((playlist) => (
+      <div key={playlist.id} className="playlist-card">
+        <div className="playlist-info">
+          <h2>{playlist.name}</h2>
+          <p>{playlist.description}</p>
+        </div>
+        <div className="playlist-icons">
+          <FontAwesomeIcon icon={faEdit} className="icon" />
+          <FontAwesomeIcon icon={faTrash} className="icon" />
+        </div>
+      </div>
+    ));
+  };
+
   return (
-    <div>
-      <h1>Playlists : </h1>
-      <button>+</button>
-    </div>
+    <div className="playlist-container">{displayPlaylists()}</div>
   )
+  
 }
 
 export default App
