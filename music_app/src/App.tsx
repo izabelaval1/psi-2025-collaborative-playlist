9645fda8c30100aaeae7f750d6951d9cf4f36e8e
Izabela <izabelaval6@gmail.com>
2025-09-17 11:35:13 +0300

    New file for playlist display

--- music_app/src/App.tsx
+++ music_app/src/App.tsx
@@ -1,40 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import PlaylistList from './PlaylistList'
 
 
 function App() {
-  const [playlists, setPlaylists]= useState([
-    { id: 1, name: 'Chill Vibes', description: 'Relaxing music' },
-    { id: 2, name: 'Workout', description: 'Pump up songs' },
-  ]);
 
-  const deletePlaylist = (id: number) => {
-    setPlaylists(playlists.filter(playlist => playlist.id !== id));
-  }
-
-  const displayPlaylists = () => {
-    return playlists.map((playlist) => (
-      <div key={playlist.id} className="playlist-card">
-        <div className="playlist-info">
-          <h2>{playlist.name}</h2>
-          <p>{playlist.description}</p>
-        </div>
-        <div className="playlist-icons">
-          <FontAwesomeIcon icon={faEdit} className="icon" />
-          <FontAwesomeIcon icon={faTrash} className="icon" 
-          onClick={() => deletePlaylist(playlist.id)}
-          />
-        </div>
-      </div>
-    ));
-  };
-
-  
 
   return (
-    <div className="playlist-container">{displayPlaylists()}</div>
+    <div><PlaylistList /></div>
   )
   
 }
