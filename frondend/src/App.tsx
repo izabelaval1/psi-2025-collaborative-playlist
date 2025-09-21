a97c27416a74278bc9ab192d070ee7005ce26a88
Emilija <emilija.rimselyte@gmail.com>
2025-09-21 11:42:34 +0300

    added backend folder

---
+++ frondend/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+import PlaylistList from './components/PlaylistList'
+
+
+function App() {
+
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  return (
+    <div>
+      
+      <MyButton onClick={() => setPopupOpen(true)} />
+      {isPopupOpen && (
+        <MyPopup onClose={() => setPopupOpen(false)} />
+      )}
+      <h1>Playlists : </h1>
+      <PlaylistList />
+    </div>
+  );
+}
+
+function MyButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button 
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg font-semibold"
+    >
+      +
+    </button>
+  );
+}
+
+function MyPopup({ onClose }: { onClose: () => void }) {
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <h2>Popup content</h2>
+        <button onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
