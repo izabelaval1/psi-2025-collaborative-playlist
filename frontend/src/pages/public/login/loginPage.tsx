39dddaf743779b22532ddc219474c15791caa19d
Izabela <izabelaval6@gmail.com>
2025-11-04 00:28:14 +0200

    added register and login pages ui

--- frontend/src/pages/public/login/loginPage.tsx
+++ frontend/src/pages/public/login/loginPage.tsx
@@ -1,8 +1,42 @@
-export default function LoginPage() {
-    return (
-      <div>
-        <h1>login here</h1>
-        <p>This will be a login page.</p>
+import styles from "./LoginPage.module.css";
+import { LogIn } from 'lucide-react';
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.loginBox}>
+        <h1 className={styles.header}>Welcome Back!</h1>
+        <p className={styles.text}>Sign in to continue creating and collaborating on playlists.</p>
+
+        <form>
+        <div className={styles.formGroup}>
+            <label>Username</label>
+            <input type="email" placeholder="Enter your username" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Email</label>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+
+          <button className={styles.signInBtn} type="submit">
+            <LogIn className={styles.icon} strokeWidth={2} size={18}/>
+            Sign In</button>
+        </form>
+
+        <p className={styles.signupText}>
+          <span className={styles.grey}>Don’t have an account?{" "}</span>
+          <Link to="/register">Sign up</Link> {" "}
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default LoginPage;
--- frontend/src/pages/public/register/registerPage.tsx
+++ frontend/src/pages/public/register/registerPage.tsx
@@ -1,8 +1,56 @@
-export default function RegisterPage() {
-    return (
-      <div>
-        <h1>register here</h1>
-        <p>This will be a register page.</p>
+import styles from "./registerPage.module.css"; 
+import { UserPlus } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const RegisterPage = () => {
+  return (
+    <div className={styles.loginPage}>
+      <div className={styles.loginBox}>
+        <h1 className={styles.header}>Create your account</h1>
+        <p className={styles.text}>
+          Start building collaborative playlists in seconds.
+        </p>
+
+        <form>
+          <div className={styles.nameRow}>
+            <div className={styles.formGroup}>
+              <label>First Name</label>
+              <input type="text" placeholder="Enter your first name" />
+            </div>
+            <div className={styles.formGroup}>
+              <label>Last Name</label>
+              <input type="text" placeholder="Enter your last name" />
+            </div>
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Email</label>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Confirm Password</label>
+            <input type="password" placeholder="Confirm your password" />
+          </div>
+
+          <button className={styles.signInBtn} type="submit">
+            <UserPlus className={styles.icon} strokeWidth={2} size={18} />
+            Sign Up
+          </button>
+        </form>
+
+        <p className={styles.signupText}>
+          <span className={styles.grey}>Already have an account?{" "}</span>
+          <Link to="/login">Log in</Link>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default RegisterPage;
